28db1323101c4d6b08684046c92b986f81e13a7b
Pierre Carrier <pc@rrier.fr>
2026-05-26 22:50:32 -0700

    Initial Commit

---
+++ zz/zzApp.swift
@@ -0,0 +1,17 @@
+//
+//  zzApp.swift
+//  zz
+//
+//  Created by Pierre Carrier on 26.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct zzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ zzTests/zzTests.swift
@@ -0,0 +1,18 @@
+//
+//  zzTests.swift
+//  zzTests
+//
+//  Created by Pierre Carrier on 26.05.2026.
+//
+
+import Testing
+
+struct zzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
